5085f72c61e555e5a40560b07ef1775bbd13455e
Andy Foster <andrewfosteris@gmail.com>
2022-09-21 15:55:55 +1200

    Set up page display

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const gridDisplay = document.querySelector('.grid');
+  const scoreDisplay = document.getElementById('score');
+  const resultDisplay = document.getElementById('result');
+});
