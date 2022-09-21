7b4a8ffefb8066521515f94b8cbc12386a11d6f0
Andy Foster <andrewfosteris@gmail.com>
2022-09-21 16:07:01 +1200

    Render width*width boxes in grid

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const gridDisplay = document.querySelector('.grid');
   const scoreDisplay = document.getElementById('score');
   const resultDisplay = document.getElementById('result');
+  const width = 4;
+  let squares = [];
+  let square;
+
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      square = document.createElement('div');
+      square.innerHTML = 0;
+      gridDisplay.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
 });
